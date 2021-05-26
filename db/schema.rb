f9fb9d26fb574f88b58f4cbeba699b9320d94feb
Atsu <kamnknon@gmail.com>
2021-05-26 22:14:07 +0900

    questionモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_26_021027) do
+ActiveRecord::Schema.define(version: 2021_05_26_130905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "content"
+    t.boolean "solved", default: false, null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_questions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -23,4 +33,5 @@ ActiveRecord::Schema.define(version: 2021_05_26_021027) do
     t.string "password_digest"
   end
 
+  add_foreign_key "questions", "users"
 end
